8f80de18a3cfafc97e98618057818bfe98b6203f
Jidim <78730440+Jidim@users.noreply.github.com>
2025-05-06 14:03:32 +0300

    init

---
+++ src/views/UserManagementView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import SidebarMenu from '@/components/SidebarMenu.vue';
+import UserTable from '@/components/UserTable.vue';
+</script>
+
+<template>
+  <main class="user-management">
+    <SidebarMenu />
+    <UserTable />
+  </main>
+</template>
+
+<style scoped lang="scss">
+.user-management {
+  background-color: var(--color-primary-900);
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  min-height: 100vh;
+}
+</style>
